a546e024ac1aae2dbd062e21dcf5c75c422f88a0
vinitasan979 <979.vini@gmail.com>
2021-01-30 04:39:23 -0800

    parse setup

---
+++ runaway/runaway/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  runaway
+//
+//  Created by Kay Lab on 1/30/21.
+//  Copyright © 2021 Vinis Prjs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
