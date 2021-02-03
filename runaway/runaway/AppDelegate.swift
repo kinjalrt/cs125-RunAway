ebd9519224dd2f787b41ac18b4b6d060d6e173d2
Maya Schwarz <mayads@uci.edu>
2021-02-02 20:42:03 -0800

    add login and sign up storyboard

--- runaway/runaway/AppDelegate.swift
+++ runaway/runaway/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
+        // Called when the user discards a scene session.#imageLiteral(resourceName: "simulator_screenshot_DCA45749-D416-4359-9939-6C8469F6D4EB.png")
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
