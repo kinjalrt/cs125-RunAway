52cbaa542cec9e8ad7c696abc93a487a3e6c37b1
Kinjal <kinjalreetoo@gmail.com>
2021-02-09 18:19:33 -0800

    weather + location homepage

--- runaway/runaway/Home.swift
+++ runaway/runaway/Home.swift
@@ -9,9 +9,117 @@
 import Foundation
 import UIKit
 import Parse
+import CoreLocation
+import Alamofire
 
-class Home: UIViewController {
+
+class Home: UIViewController, CLLocationManagerDelegate {
+    
+    let LocationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var weatherSummary: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpLocation()
+    }
+    
+    
+    // Location
+    func setUpLocation() {
+        LocationManager.delegate = self
+        LocationManager.requestWhenInUseAuthorization()
+        LocationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation==nil{
+            currentLocation = locations.first
+            LocationManager.stopUpdatingLocation()
+            displayCityForLocation()
+            requestWeatherForLocation()
+        }
+    }
+    
+    func displayCityForLocation() {
+            // Use the last reported location.
+            guard let currentLocation = currentLocation else{
+                return
+            }
+            let long = currentLocation.coordinate.longitude
+            let lat = currentLocation.coordinate.latitude
+            let geocoder = CLGeocoder()
+            let location = CLLocation(latitude: lat, longitude: long)
+            var currentCity = ""
+            var currentState = ""
+            
+            geocoder.reverseGeocodeLocation(location, completionHandler:
+                {
+                    placemarks, error -> Void in
+
+                    // Place details
+                    guard let placeMark = placemarks?.first else { return }
+
+                    // city
+                    if (placeMark.locality != nil) {
+                        print(currentCity)
+                        currentCity = placeMark.locality ?? ""
+                    }
+                    // state
+                    if (placeMark.administrativeArea != nil){
+                        currentState = placeMark.administrativeArea ?? ""
+                    }
+                    
+                    self.city.text = currentCity+", "+currentState
+                    
+            })
+            
+        
+    }
+    
+    func requestWeatherForLocation(){
+        guard let currentLocation = currentLocation else{
+            return
+        }
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        print("\(long) | \(lat)")
+        
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(long)&units=imperial&appid=64611f4ad8a75ee7950a4befef783919")!
+       
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            print(data)
+            let main = dataDictionary["main"] as! [String:Any]
+            let temp = main["temp"] as! NSNumber
+            print(temp)
+            let sumObj = dataDictionary["weather"] as! NSArray
+            let summary = sumObj[0] as! NSDictionary
+            let description = summary["description"] as! String
+            print(description)
+
+            self.temperature.text = temp.stringValue+"°F"
+            self.weatherSummary.text = description
+
+           }
+        }
+        task.resume()
+        
+        
+    }
+    
+    
     
     
 }
+
+
 
