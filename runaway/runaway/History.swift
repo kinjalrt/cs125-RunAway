77b08bcd2aecf872b24c9505518685efe68d51c2
vinitasan979 <979.vini@gmail.com>
2021-02-15 05:41:02 -0800

    history page

---
+++ runaway/runaway/History.swift
@@ -0,0 +1,44 @@
+//
+//  History.swift
+//  runaway
+//
+//  Created by Kay Lab on 2/15/21.
+//  Copyright © 2021 Vinis Prjs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+
+class History:UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var runsCompleted: UILabel!
+    @IBOutlet weak var timeCompleted: UILabel!
+    @IBOutlet weak var distCompleted: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        var currentUser = PFUser.current()
+        //set up greeting
+        var greeting="Hello "
+        greeting+=currentUser?["firstname"] as! String
+        username.text=greeting
+        //runs
+        var runs=String(currentUser?["totalRuns"] as! Int)
+        runs+=" runs completed"
+        
+        var time=String(currentUser?["totalTime"] as! Int)
+        time+=" minutes total"
+        
+        
+
+    }
+    
+}
