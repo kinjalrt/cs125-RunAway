7b9074c87039bd318af106128defe5a6d0825757
Kinjal <kinjalreetoo@gmail.com>
2021-02-25 22:33:15 -0800

    retrieve access token + segments

--- starvatester/starvatester/ViewController.swift
+++ starvatester/starvatester/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AuthenticationServices
+import Alamofire
 
 class ViewController: UIViewController {
     
@@ -15,13 +16,16 @@ class ViewController: UIViewController {
 
     private let clientId: String = "61588"
     private let urlScheme: String = "testerapp"
-    private let fallbackUrl: String = "http://localhost/"
+    private let fallbackUrl: String = "testerapp.com"
+    private let clientSecret: String = "4c5efd4b78648d6f3f21a4089c54d1bdfb49f8a6"
+    var code: String = ""
 
        
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func connectStrava(_ sender: Any) {
@@ -30,13 +34,6 @@ class ViewController: UIViewController {
     
     private func presentStrava(){
         
-       // https://www.strava.com/oauth/mobile/authorize?client_id=1234321&redirect_uri= YourApp%3A%2F%2Fwww.yourapp.com%2Fen-US&response_type=code&approval_prompt=auto&scope=activity%3Awrite%2Cread&state=test")!
-
-        
-        //let url: String = "https://www.strava.com/oauth/mobile/authorize?client_id=61588&redirect_uri= testerapp%3A%2F%2Fhttp://localhost/%2Fen-US&response_type=code&approval_prompt=auto&scope=activity%3Awrite%2Cread&state=test"
-
-        
-        //let url: String = "http://www.strava.com/oauth/authorize?client_id=\(clientId)&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read"
         let url: String = "https://www.strava.com/oauth/mobile/authorize?client_id=\(clientId)&redirect_uri=\(urlScheme)%3A%2F%2F\(fallbackUrl)&response_type=code&approval_prompt=auto&scope=read"
         guard let authenticationUrl = URL(string: url) else { return }
         print(url)
@@ -45,13 +42,20 @@ class ViewController: UIViewController {
                    if let error = error {
                        print(error)
                    } else {
-                       if let url = url {
-                           print(url)
+                    if let code = self.getCode(from: url) {
+                           print("sasuke")
+                           print(code)
+                        self.requestStravaTokens(with: code)
+                        
                        }
                    }
                }
         authSession?.presentationContextProvider = self
         authSession?.start()
+        
+        
+        
+        
     }
     
     private func getCode(from url: URL?) -> String? {
@@ -61,6 +65,31 @@ class ViewController: UIViewController {
         let code: String? = urlComponents?.queryItems?.filter { $0.name == "code" }.first?.value
         
         return code
+    }
+    
+    private func requestStravaTokens(with code: String) {
+        let parameters: [String: Any] = ["client_id": clientId, "client_secret": clientSecret, "code": code, "grant_type": "authorization_code"]
+
+        AF.request("https://www.strava.com/oauth/token", method: .post, parameters: parameters).response { response in
+            guard let data = response.data else { return }
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            print("cat")
+            let accessToken = dataDictionary["access_token"] as! String
+            print(accessToken)
+            self.retrieveSegments(with: accessToken)
+        }
+    }
+    
+    private func retrieveSegments(with accessToken: String){
+        let parameters: [String: Any] = ["access_token": accessToken, "bounds": "[37.331193,-122.031401,40.331193,-119.031401]", "activity_type": "running"]
+
+        AF.request("https://www.strava.com/api/v3/segments/explore", method: .get, parameters: parameters).response { response in
+            guard let data = response.data else { return }
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            print(dataDictionary)
+        }
+        
+        
     }
     
   
