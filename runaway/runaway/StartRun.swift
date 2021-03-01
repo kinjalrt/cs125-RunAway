32931121d521ef419fe98562ff087821c7630278
jcallant <jcallant@uci.edu>
2021-03-01 04:36:21 -0800

    Fixed some redundancy; setup filtering

--- runaway/runaway/StartRun.swift
+++ runaway/runaway/StartRun.swift
@@ -13,6 +13,8 @@ import Alamofire
 
 
 class StartRun: UIViewController {
+    
+    // UI Components
     @IBOutlet weak var distanceSlider: UISlider!
     @IBOutlet weak var inclineSlider: UISlider!
     @IBOutlet weak var distanceLabel: UILabel!
@@ -27,30 +29,129 @@ class StartRun: UIViewController {
     @IBOutlet weak var routeDistance: UILabel!
     
     
-    
-    
+    // Logic Components
     var routesSegments: [Segments] = [] //array of routes
-    
-
-    
+    var routesSegmentIds: [Int] = []
+    var filteredRouteSegments: [Segments] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getRuns()
-        
+        getRoutes()
+        //print(self.routesSegments)
     }
 
     
     @IBAction func distanceSliderValueChanged(_ sender: UISlider) {
         let currentValue = Int(sender.value)
-            
         distanceLabel.text = "\(currentValue)"
+    }
+    
+    @IBAction func distanceSliderValueChosen(_ sender: UISlider) {
+        filterRoutesByDistance(distance: Double(sender.value) )
+        print("\nFiltered Routes (Id, TotalDistance, DistanceAway):")
+        for r in filteredRouteSegments{
+            let distanceInMiles = r.distance / 1000 * 0.621371
+            let distanceAway = r.distanceAway / 1000 * 0.621371
+            print(String(format: "%10.d \t %8.2f miles \t %8.2f", r.stravaDataId, distanceInMiles, distanceAway))
+        }
     }
     
     @IBAction func inclineSliderValueChanged(_ sender: UISlider) {
         let currentValue = Int(sender.value)
             
         inclineLabel.text = "\(currentValue)"
+    }
+    
+    
+    func getRoutes() {
+        let long = Home.currentLocation!.coordinate.longitude
+        let lat = Home.currentLocation!.coordinate.latitude
+        
+        // Not sure how the bounds are supposed to be (fix this if needed)
+        let northeastBounds = "[\(lat),\(long),\(lat+3),\(long+3)]"
+        let northwestBounds = "[\(lat),\(long-3),\(lat+3),\(long)]"
+        let southwestBounds = "[\(lat-3),\(long-3),\(lat),\(long)]"
+        let southeastBounds = "[\(lat-3),\(long),\(lat),\(long+3)]"
+        
+        print("\n")
+        getRoutesFromBounds(bounds: northeastBounds, directionString: "NORTH_EAST")
+        getRoutesFromBounds(bounds: northwestBounds, directionString: "NORTH_WEST" )
+        getRoutesFromBounds(bounds: southwestBounds, directionString: "SOUTH_WEST")
+        getRoutesFromBounds(bounds: southeastBounds, directionString: "SOUTH_EAST")
+    }
+    
+    
+    func getRoutesFromBounds(bounds: String, directionString: String){
+        let accessToken = LogIn.accessToken
+        let parameters: [String: Any] = ["access_token": accessToken, "bounds": bounds, "activity_type": "running"]
+        AF.request("https://www.strava.com/api/v3/segments/explore", method: .get, parameters: parameters).response { response in
+            guard let data = response.data else { return }
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            print("ALL NEW SEGMENTS IN \(directionString) AREA: ")
+            var count=0
+            let seg = dataDictionary["segments"] as! [AnyObject]
+                for key in seg{
+                    
+                    // Prevent duplicates
+                    let id = key["id"] as! Int
+                    if self.routesSegmentIds.contains(id){
+                        continue
+                    }
+                    
+                    //print(dataDictionary)
+                    let dist = key["distance"] as! Double
+                    let s = key["start_latlng"] as! NSArray
+                    let e = key["end_latlng"] as! NSArray
+                    
+                    //making coords for starting and ending point
+                    let start_loc = CLLocation(latitude: s[0] as! CLLocationDegrees, longitude: s[1] as! CLLocationDegrees)
+                    let end_loc = CLLocation(latitude: e[0] as! CLLocationDegrees, longitude: e[1] as! CLLocationDegrees)
+                    let distanceAway = self.getDistanceAway(location: start_loc)
+                    
+                    // make segement object
+                    let curr_seg = Segments(stravaDataId: id, distance: dist, distanceAway: distanceAway, start: start_loc, end: end_loc)
+                
+                    //let curr_seg = Segments(d:dist, slat: s[0] as! Double, slong: s[1] as! Double, elat: e[0] as! Double, elong: e[1] as! Double)
+                    self.routesSegments.append(curr_seg)
+                    self.routesSegmentIds.append(id)
+                    count+=1
+                    
+                }
+            self.sortSegments()
+            //print(self.routesSegments)
+        }
+    }
+    
+    func getDistanceAway(location: CLLocation) -> Double{
+        let userLoc = CLLocation(
+            latitude:Home.currentLocation!.coordinate.latitude,
+            longitude:Home.currentLocation!.coordinate.longitude
+        )
+        return location.distance(from: userLoc)
+    }
+    
+    func sortSegments(){
+//        let userLoc: CLLocation = CLLocation(latitude:Home.currentLocation!.coordinate.latitude,longitude:Home.currentLocation!.coordinate.longitude)
+//
+//        let sortedSegs = self.routesSegments.sorted(by: {$0.startLoc.distance(from: userLoc) < $1.startLoc.distance(from: userLoc)})
+//        self.routesSegments = sortedSegs
+        let sortedSegs = self.routesSegments.sorted(by: {$0.distanceAway < $1.distanceAway})
+        self.routesSegments = sortedSegs
+        print("\tsorted count = \(sortedSegs.count) normal count = \(self.routesSegments.count)")
+    }
+    
+    func filterRoutesByDistance(distance: Double){
+        // reset filtered segments
+        filteredRouteSegments = []
+        
+        for r in routesSegments{
+            
+            //convert meters to miles
+            let distanceInMiles = r.distance / 1000 * 0.621371
+            if distanceInMiles <= distance{
+                filteredRouteSegments.append(r)
+            }
+        }
     }
     
     func displayRoutes(){
@@ -87,18 +188,13 @@ class StartRun: UIViewController {
         
         //get distance + name of route from Strava API
         self.routeDistance.text = "2 miles"
-        
-    
     }
     
     func findRoutes(){
         //get routes from Strava API:
         
         //create CLLocationCoordinate2D object for each route and append to self.suggestedRoutes
-        
-        
-        
-        
+
         //sample routes:
         var destCoordinates = CLLocationCoordinate2D(latitude: 37.331193,longitude: -122.031401)
         self.suggestedRoutes.append(destCoordinates)
@@ -106,13 +202,12 @@ class StartRun: UIViewController {
         self.suggestedRoutes.append(destCoordinates)
         destCoordinates = CLLocationCoordinate2D(latitude: 37.326644,longitude: -122.030186)
         self.suggestedRoutes.append(destCoordinates)
-        
+
         //init navig buttons
         prevButton.isHidden = true
         if(self.suggestedRoutes.count==1){
             prevButton.isHidden = true
         }
-
     }
     
     
@@ -156,176 +251,5 @@ class StartRun: UIViewController {
     }
     
     
-     func getRuns() {
-        let long = Home.currentLocation!.coordinate.longitude
-        let lat = Home.currentLocation!.coordinate.latitude
-        let accessToken = LogIn.accessToken
-        
-        //retrieve all routes in North-East area relative to current location
-        let NElat = lat+3
-        let NElong = long+3
-        let northeastBounds = "[\(lat),\(long),\(NElat),\(NElong)]"
-        var parameters: [String: Any] = ["access_token": accessToken, "bounds": northeastBounds, "activity_type": "running"]
-        
-        AF.request("https://www.strava.com/api/v3/segments/explore", method: .get, parameters: parameters).response { response in
-            guard let data = response.data else { return }
-            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            print("ALL SEGMENTS IN NORTH-EAST AREA: ")
-            var count=0
-            let seg = dataDictionary["segments"] as! [AnyObject]
-                for key in seg{
 
-                    let dist = key["distance"] as! Double
-                    let s = key["start_latlng"] as! NSArray
-                    let e = key["end_latlng"] as! NSArray
-                    
-                    //making coords for starting and ending point
-                    let start_loc = CLLocation(latitude: s[0] as! CLLocationDegrees, longitude: s[1] as! CLLocationDegrees)
-                    let end_loc = CLLocation(latitude: e[0] as! CLLocationDegrees, longitude: e[1] as! CLLocationDegrees)
-                    
-                    // make segement object
-                    let curr_seg = Segments(d:dist, start: start_loc, end: end_loc)
-                
-                    //let curr_seg = Segments(d:dist, slat: s[0] as! Double, slong: s[1] as! Double, elat: e[0] as! Double, elong: e[1] as! Double)
-                    self.routesSegments.append(curr_seg)
-                    count+=1
-                    
-                    
-                }
-            print("ne",count)
-            self.sortSegments()
-
-            //print(self.routesSegments)
-            
-        }
-        
-        //retrieve all routes in North-West area relative to current location
-        let NWlat = lat-3
-        let NWlong = long+3
-        let NWcurrLat = lat-3
-        let northwestBounds = "[\(NWcurrLat),\(long),\(NWlat),\(NWlong)]"
-        parameters = ["access_token": accessToken, "bounds": northwestBounds, "activity_type": "running"]
-        
-        AF.request("https://www.strava.com/api/v3/segments/explore", method: .get, parameters: parameters).response { response in
-            guard let data = response.data else { return }
-            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            print("ALL SEGMENTS IN NORTH-WEST AREA: ")
-            //print(dataDictionary)
-            var count=0
-            let seg = dataDictionary["segments"] as! [AnyObject]
-                for key in seg{
-
-                    let dist = key["distance"] as! Double
-                    let s = key["start_latlng"] as! NSArray
-                    let e = key["end_latlng"] as! NSArray
-                    //making coords for starting and ending point
-                    let start_loc = CLLocation(latitude: s[0] as! CLLocationDegrees, longitude: s[1] as! CLLocationDegrees)
-                    let end_loc = CLLocation(latitude: e[0] as! CLLocationDegrees, longitude: e[1] as! CLLocationDegrees)
-                    
-                    // make segement object
-                    let curr_seg = Segments(d:dist, start: start_loc, end: end_loc)
-                    
-                   // let curr_seg = Segments(d:dist, slat: s[0] as! Double, slong: s[1] as! Double, elat: e[0] as! Double, elong: e[1] as! Double)
-                    self.routesSegments.append(curr_seg)
-                    count+=1
-                    
-                    
-                }
-            print("nw",count)
-            self.sortSegments()
-
-        }
-        
-        
-        //retrieve all routes in South-West area relative to current location
-        let SWlat = lat-3
-        let SWlong = long-3
-        let southwestBounds = "[\(SWlat),\(SWlong),\(lat),\(long)]"
-        parameters = ["access_token": accessToken, "bounds": southwestBounds, "activity_type": "running"]
-        
-        AF.request("https://www.strava.com/api/v3/segments/explore", method: .get, parameters: parameters).response { response in
-            guard let data = response.data else { return }
-            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            print("ALL SEGMENTS IN SOUTH-WEST AREA: ")
-            var count=0
-            let seg = dataDictionary["segments"] as! [AnyObject]
-                for key in seg{
-
-                    let dist = key["distance"] as! Double
-                    let s = key["start_latlng"] as! NSArray
-                    let e = key["end_latlng"] as! NSArray
-                    //making coords for starting and ending point
-                    let start_loc = CLLocation(latitude: s[0] as! CLLocationDegrees, longitude: s[1] as! CLLocationDegrees)
-                    let end_loc = CLLocation(latitude: e[0] as! CLLocationDegrees, longitude: e[1] as! CLLocationDegrees)
-                    
-                    //let curr_seg = Segments(d:dist, slat: s[0] as! Double, slong: s[1] as! Double, elat: e[0] as! Double, elong: e[1] as! Double)
-                    
-                    // make segement object
-                    let curr_seg = Segments(d:dist, start: start_loc, end: end_loc)
-                    self.routesSegments.append(curr_seg)
-                    count+=1
-                    
-                    
-                }
-            print("sw",count)
-            self.sortSegments()
-
-            //print(dataDictionary)
-        }
-        
-        //retrieve all routes in South-East area relative to current location
-        let SElat = lat+3
-        let SElong = long-3
-        let southeastBounds = "[\(lat),\(SElong),\(SElat),\(long)]"
-        parameters = ["access_token": accessToken, "bounds": southeastBounds, "activity_type": "running"]
-        
-        AF.request("https://www.strava.com/api/v3/segments/explore", method: .get, parameters: parameters).response { response in
-            guard let data = response.data else { return }
-            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            print("ALL SEGMENTS IN SOUTH-EAST AREA: ")
-            var count=0
-            let seg = dataDictionary["segments"] as! [AnyObject]
-                for key in seg{
-
-                    let dist = key["distance"] as! Double
-                    let s = key["start_latlng"] as! NSArray
-                    let e = key["end_latlng"] as! NSArray
-                    //making coords for starting and ending point
-                    let start_loc = CLLocation(latitude: s[0] as! CLLocationDegrees, longitude: s[1] as! CLLocationDegrees)
-                    let end_loc = CLLocation(latitude: e[0] as! CLLocationDegrees, longitude: e[1] as! CLLocationDegrees)
-                    
-                    //let curr_seg = Segments(d:dist, slat: s[0] as! Double, slong: s[1] as! Double, elat: e[0] as! Double, elong: e[1] as! Double)
-                    
-                    // make segement object
-                    let curr_seg = Segments(d:dist, start: start_loc, end: end_loc)
-                    self.routesSegments.append(curr_seg)
-                    count+=1
-                    
-                    
-                }
-            print("se",count)
-            self.sortSegments()
-
-           // print("final",self.routesSegments)
-        }
-        
-        
-        
-        
-        
-        
-    }
-    
-    func sortSegments(){
-        
-        let userLoc: CLLocation = CLLocation(latitude:Home.currentLocation!.coordinate.latitude,longitude:Home.currentLocation!.coordinate.longitude)
-        
-        let sortedSegs = self.routesSegments.sorted(by: {$0.startLoc.distance(from: userLoc) < $1.endLoc.distance(from: userLoc)})
-        self.routesSegments = sortedSegs
-        print("sorted count = \(sortedSegs.count) normal count = \(self.routesSegments.count)")
-        
-        
-    }
-
-    
 }
