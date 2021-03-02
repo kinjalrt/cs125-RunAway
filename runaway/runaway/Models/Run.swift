c8638b0342d1625ce95c712b204c857de5e7df0f
jcallant <jcallant@uci.edu>
2021-03-02 00:58:11 -0800

    modified for easy construction

--- runaway/runaway/Models/Run.swift
+++ runaway/runaway/Models/Run.swift
@@ -11,6 +11,7 @@ import Parse
 
 
 class Run {
+    var objectId: String
     var routeId: String
     var userId: String
     var startTimeStamp: NSDate
@@ -20,17 +21,44 @@ class Run {
     
     // Run object should created only AFTER run is complete
     init(routeId: String, userId: String, startTimeStamp: NSDate, endTimeStamp: NSDate, runName: String){
+        self.objectId = ""
         self.routeId = routeId
         self.userId = userId
         self.startTimeStamp = startTimeStamp
         self.endTimeStamp = endTimeStamp
         self.runName = runName
         
-        self.pushToDatabase()
+        self.objectId = self.pushToDatabase()
     }
     
     
-    private func pushToDatabase(){
+    init(objectId: String){
+        self.objectId = ""
+        self.routeId = ""
+        self.userId = ""
+        self.startTimeStamp = NSDate()
+        self.endTimeStamp = NSDate()
+        self.runName = ""
+        
+        let query = PFQuery(className: "Run")
+        query.whereKey("objectId", equalTo: objectId)
+        query.findObjectsInBackground{ (runs, error) in
+            if error != nil {
+                print("Error: Could not find route in database.")
+            }
+            else if runs?.count != 0 {
+                self.objectId = runs![0]["objectId"] as! String
+                self.routeId = runs![0]["routeId"] as! String
+                self.userId = runs![0]["userId"] as! String
+                self.startTimeStamp = runs![0]["startTimeStamp"] as! NSDate
+                self.endTimeStamp = runs![0]["endTimeStamp"] as! NSDate
+                self.runName = runs![0]["runName"] as! String
+            }
+        }
+    }
+    
+    
+    private func pushToDatabase() -> String{
         let parseObject = PFObject(className: "Run")
 
         parseObject["routeId"] = self.routeId
@@ -40,14 +68,17 @@ class Run {
         parseObject["runName"] = self.runName
         
         // Saves the new object.
+        var id = self.objectId
         parseObject.saveInBackground {
           (success: Bool, error: Error?) in
           if (success) {
             print("Successfully pushed RUN to database.")
+            id = parseObject.objectId!
           } else {
             print("Error: Could not push RUN to database.")
           }
         }
+        return id
     }
     
     
