2f64ec2061e1665efa5a2161b698bc6b6e48db6b
jcallant <jcallant@uci.edu>
2021-03-02 06:52:43 -0800

    stopButton triggers Run creation

--- runaway/runaway/RunStatus.swift
+++ runaway/runaway/RunStatus.swift
@@ -11,24 +11,28 @@ import UIKit
 import Parse
 
 class RunStatus: UIViewController {
+    var route = PFObject(className: "Route")
+    var routeName = ""
     var counter = 0.0
     var timer = Timer()
     var isPlaying = true
+    var startTime = NSDate()
     
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pauseButton.isEnabled = true
-        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        
+        self.stopButton.isEnabled = true
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
         isPlaying = true
         timeLabel.text = String(counter)
     }
         
-    @IBAction func pauseTimer(_ sender: AnyObject) {
-        pauseButton.isEnabled = false
-            
+    @IBAction func stopTimer(_ sender: AnyObject) {
+        createRun()
+        stopButton.isEnabled = false
         timer.invalidate()
         isPlaying = false
     }
@@ -36,6 +40,41 @@ class RunStatus: UIViewController {
     @objc func UpdateTimer() {
         counter = counter + 0.1
         timeLabel.text = String(format: "%.1f", counter)
+    }
+    
+    func createRun() {
+        self.route.incrementKey("totalRuns")
+        let endTime = NSDate()
+        let elapsedTime = endTime.timeIntervalSince(self.startTime as Date)
+        
+        let run = PFObject(className: "Run")
+        run["route"] = self.route
+        run["user"] = PFUser.current()!
+        run["startTimeStamp"] = self.startTime
+        run["totalDistance"] = self.route["distance"] as! Double
+        run["elapsedTime"] = elapsedTime
+        run["runName"] = self.routeName
+        run.saveInBackground{
+            (success: Bool, error: Error?) in
+            if (success) {
+              print("Successfully pushed RUN to database.")
+                let user = PFUser.current() as! User
+                user.add(run, forKey: "listOfRuns")
+                user.incrementKey("totalRuns")
+                user.incrementKey("totalTime", byAmount: elapsedTime as NSNumber)
+                user.incrementKey("totalMiles", byAmount: (self.route["distance"] as! Double / 1000 * 0.621371) as NSNumber)
+                user.saveInBackground {
+                  (success: Bool, error: Error?) in
+                  if (success) {
+                    print("Successfully added RUN to USER.listOfRuns in database.")
+                  } else {
+                    print("Error: Could not add RUN to USER.listOfRuns in database.")
+                  }
+                }
+            } else {
+              print("Error: Could not push RUN to database.")
+            }
+          }
     }
 }
 
