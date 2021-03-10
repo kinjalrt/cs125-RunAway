2da434e83e544b39be832cf1fe3ae624428b8866
Maya Schwarz <39718959+mayaschwarz@users.noreply.github.com>
2021-03-10 14:51:27 -0800

    Maya/mar10/runstatusui (#12)
    * ui updates to run status
    
    * more run status stuff

--- runaway/runaway/RunStatus.swift
+++ runaway/runaway/RunStatus.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 import Parse
 
-class RunStatus: UIViewController {
+class RunStatus: UIViewController, CLLocationManagerDelegate {
     var route = PFObject(className: "Route")
     var routeName = ""
     var routeDist = 0.0
@@ -20,9 +20,9 @@ class RunStatus: UIViewController {
     var elapsedTime = 0.0
     var breaks = 0
     var (minutes,seconds,frac)=(0,0,0)
+    let LocationManager = CLLocationManager()
     
-
-    
+    @IBOutlet weak var temperature: UILabel!
     @IBOutlet weak var fracLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
@@ -31,17 +31,58 @@ class RunStatus: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         self.stopButton.isEnabled = true
         self.resumeButton.isHidden = true
-        //update time every 0.01 seconds using the update timer function
-        self.timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(RunStatus.UpdateTimer), userInfo: nil, repeats: true)
+        requestWeatherForLocation()
+        // change timer fonts to monospaced digits to avoid jostling numbers
+        timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 90, weight: UIFont.Weight.regular)
+        fracLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 90, weight: UIFont.Weight.regular)
+        // update time every 0.01 seconds using the update timer function
+        timer = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        timeLabel.text = String(counter)
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    // Get location so we can call the API to get the weather
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, Home.currentLocation==nil{
+            Home.currentLocation = locations.first
+            LocationManager.stopUpdatingLocation()
+        }
+    }
+    
+    // API call to request weather so we can display the current weather during the run
+    
+    func requestWeatherForLocation(){
+        guard let currentLocation = Home.currentLocation else{
+            return
+        }
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(long)&units=imperial&appid=64611f4ad8a75ee7950a4befef783919")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            let main = dataDictionary["main"] as! [String:Any]
+            let temp = main["temp"] as! NSNumber
+
+            self.temperature.text = "currently "+temp.stringValue+"°F"
+           }
+        }
+        task.resume()
+    
     }
         
     @IBAction func stopTimer(_ sender: AnyObject) {
@@ -57,17 +98,17 @@ class RunStatus: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //send data from this page to post survey page to calculate score
-        var vc = segue.destination as! PostRunSurvey
+        let vc = segue.destination as! PostRunSurvey
         vc.breaks = self.breaks
         vc.totaltime = self.elapsedTime
         vc.routeName = self.routeName
         vc.route = self.route
         vc.routeDist = self.routeDist
-        
     }
     
+    //update time and format for user display
+    
     @objc func UpdateTimer() {
-        //update time and format for user display
         frac+=1
         if frac > 60 {
             seconds+=1
@@ -87,23 +128,26 @@ class RunStatus: UIViewController {
      
     }
     
+    // functionality to pause timer for break
+    
     @IBAction func PauseTimer(_ sender: Any) {
         //each time user pauses the timer, it counts as a break
         timer.invalidate()
         breaks+=1
-        //enable resume button so user can continue when ready
+        
+        //enable resume button so user can continue after the break
         self.resumeButton.isHidden = false
         self.pauseButton.isHidden = true
         
-        
     }
+    
+    // functionality to restart/resume timer after runner takes a break
     
     @IBAction func ResumeTimer(_ sender: Any) {
         //restart timer from where it was stopped
-        self.timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(RunStatus.UpdateTimer), userInfo: nil, repeats: true)
+        self.timer = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector(RunStatus.UpdateTimer), userInfo: nil, repeats: true)
         self.pauseButton.isHidden = false
         self.resumeButton.isHidden = true
-
 
     }
     
